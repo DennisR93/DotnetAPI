f46b2de2635e07f81f113c377638dcbfe96b5991
dennisrabinovitch <rdennis0993@gmail.com>
2024-03-07 08:37:52 +0200

    creating entitiy framework controller

---
+++ DotnetAPI/Controllers/UserEfController.cs
@@ -0,0 +1,96 @@
+using DotnetAPI.Data;
+using DotnetAPI.DTOs;
+using DotnetAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserEFController : ControllerBase
+{
+    private DataContextEF _entityFramework;
+    public UserEFController(IConfiguration config)
+    {
+        _entityFramework = new DataContextEF(config);
+    }
+    
+    [HttpGet("GetUsers")]
+    public IEnumerable<User> GetUsers()
+    {
+        IEnumerable<User> users = _entityFramework.Users.ToList<User>();
+        return users;
+
+    }
+    
+    [HttpGet("GetSingleUser/{userId}")]
+    public User GetSingleUser(int userId)
+    {
+        User? user = _entityFramework.Users.Where(u => u.UserId == userId).FirstOrDefault<User>();
+        if (user != null)
+        {
+            return user;
+        }
+
+        throw new Exception("Failed to Get User");
+    }
+
+    [HttpPut("EditUser")]
+    public IActionResult EditUser(User user)
+    {
+        User? userDb = _entityFramework.Users.Where(u => u.UserId == user.UserId).FirstOrDefault<User>();
+        if (userDb != null)
+        {
+            userDb.Active = user.Active;
+            userDb.FirstName = user.FirstName;
+            userDb.LastName = user.LastName;
+            userDb.Gender = user.Gender;
+            userDb.Email = user.Email;
+
+            if (_entityFramework.SaveChanges() > 0)
+            {
+                return Ok();
+            }
+        }
+
+        throw new Exception("Failed to Get User");
+    }
+
+    [HttpPost("AddUser")]
+    public IActionResult AddUser(UserToAddDto userToAdd)
+    {
+
+            User userDb = new User();
+        
+            userDb.Active = userToAdd.Active;
+            userDb.FirstName = userToAdd.FirstName;
+            userDb.LastName = userToAdd.LastName;
+            userDb.Gender = userToAdd.Gender;
+            userDb.Email = userToAdd.Email;
+            
+            _entityFramework.Add(userDb);
+            if (_entityFramework.SaveChanges() > 0)
+            {
+                return Ok();
+            }
+            
+            throw new Exception("Failed to Add User");
+    }
+
+    [HttpDelete("DeleteUser/{userId}")]
+    public IActionResult DeleteUser(int userId)
+    {
+        User? userDb = _entityFramework.Users.Where(u => u.UserId == userId).FirstOrDefault<User>();
+        if (userDb != null)
+        {
+            _entityFramework.Users.Remove(userDb);
+            if (_entityFramework.SaveChanges() > 0)
+            {
+                return Ok();
+            }
+            throw new Exception("Failed to Delete User");
+        }
+        throw new Exception("Failed to Get User");
+    }
+    
+}
