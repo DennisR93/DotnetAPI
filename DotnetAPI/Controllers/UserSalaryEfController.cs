d117145264d1d32e9033cb3d2010634a1ff4a254
dennisrabinovitch <rdennis0993@gmail.com>
2024-03-07 17:55:31 +0200

    refactoring

--- DotnetAPI/Controllers/UserSalaryEfController.cs
+++ DotnetAPI/Controllers/UserSalaryEfController.cs
@@ -18,7 +18,7 @@ public class UserSalaryEfController : ControllerBase
         _entityFramework = new DataContextEF(config);
         _mapper = new Mapper(new MapperConfiguration(cfg =>
         {
-            cfg.CreateMap<UserSalaryToAddDto, UserSalary>();
+            cfg.CreateMap<UserSalary, UserSalary>();
         }));
     }
 
@@ -44,30 +44,30 @@ public class UserSalaryEfController : ControllerBase
     [HttpPut("EditUserSalary")]
     public IActionResult EditUserSalary(UserSalary userSalary)
     {
-        UserSalary? userDb = _entityFramework.UserSalary.Where(u => u.UserId == userSalary.UserId).FirstOrDefault();
-        if (userDb != null)
+        UserSalary? userSalaryToUpdate = _entityFramework.UserSalary.Where(u => u.UserId == userSalary.UserId).FirstOrDefault();
+        if (userSalaryToUpdate != null)
         {
-            userDb.Salary = userSalary.Salary;
-            userDb.AvgSalary = userSalary.AvgSalary;
-
+            _mapper.Map(userSalary, userSalaryToUpdate);
             if (_entityFramework.SaveChanges() > 0)
             {
                 return Ok();
             }
+
+            throw new Exception("Updating UserSalary failed to save");
         }
 
-        throw new Exception("Failed to Edit User Salary");
+        throw new Exception("Failed to Find UserSalary to Update");
     }
 
     [HttpPost("AddUserSalary")]
-    public IActionResult AddUserSalary(UserSalaryToAddDto userSalaryToAdd)
+    public IActionResult AddUserSalary(UserSalary userSalaryToAdd)
     {
-        UserSalary userDb = _mapper.Map<UserSalary>(userSalaryToAdd);
+        // UserSalary userDb = _mapper.Map<UserSalary>(userSalaryToAdd);
+        //
+        // userDb.Salary = userSalaryToAdd.Salary;
+        // userDb.AvgSalary = userSalaryToAdd.AvgSalary;
 
-        userDb.Salary = userSalaryToAdd.Salary;
-        userDb.AvgSalary = userSalaryToAdd.AvgSalary;
-
-        _entityFramework.Add(userDb);
+        _entityFramework.Add(userSalaryToAdd);
         if (_entityFramework.SaveChanges() > 0)
         {
             return Ok();
@@ -79,17 +79,17 @@ public class UserSalaryEfController : ControllerBase
     [HttpDelete("DeleteUserJSalary/{userId}")]
     public IActionResult DeleteUserSalary(int userId)
     {
-        UserSalary? userDb = _entityFramework.UserSalary.Where(u => u.UserId == userId).FirstOrDefault<UserSalary>();
-        if (userDb != null)
+        UserSalary? userToDelete = _entityFramework.UserSalary.Where(u => u.UserId == userId).FirstOrDefault<UserSalary>();
+        if (userToDelete != null)
         {
-            _entityFramework.UserSalary.Remove(userDb);
+            _entityFramework.UserSalary.Remove(userToDelete);
             if (_entityFramework.SaveChanges() > 0)
             {
                 return Ok();
             }
-            throw new Exception("Failed to Delete User Job Info");
+            throw new Exception("Failed to Delete UserSalary Info");
         }
 
-        throw new Exception("Failed to Get User Job Info");
+        throw new Exception("Failed to Get UserSalary to Delete");
     }
 }
