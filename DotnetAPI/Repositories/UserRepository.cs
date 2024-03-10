0556fdaf9d4b85e1e98ea609e40caca6c5bdfd6e
dennisrabinovitch <rdennis0993@gmail.com>
2024-03-10 22:45:04 +0200

    adding methods

--- DotnetAPI/Repositories/UserRepository.cs
+++ DotnetAPI/Repositories/UserRepository.cs
@@ -1,4 +1,5 @@
 using DotnetAPI.Interfaces;
+using DotnetAPI.Models;
 
 namespace DotnetAPI.Data;
 
@@ -30,6 +31,49 @@ public class UserRepository : IUserRepository
         {
             _entityFramework.Remove(entityToRemove);
         }
+    }
+    
+    public IEnumerable<User> GetUsers()
+    {
+        IEnumerable<User> users = _entityFramework.Users.ToList<User>();
+        return users;
+
+    }
+    
+    public User GetSingleUser(int userId)
+    {
+        User? user = _entityFramework.Users.Where(u => u.UserId == userId).FirstOrDefault<User>();
+        if (user != null)
+        {
+            return user;
+        }
+
+        throw new Exception("Failed to Get User");
+    }
+    
+    //UserSalary
+    
+    public UserSalary GetSingleUserSalary(int userId)
+    {
+        UserSalary? userSalary = _entityFramework.UserSalary.Where(u => u.UserId == userId).FirstOrDefault();
+        if (userSalary != null)
+        {
+            return userSalary;
+        }
+
+        throw new Exception("Failed to Get User Salary");
+    }
+    
+    //UserJobInfo
+    public UserJobInfo GetSingleUserJobInfo(int userId)
+    {
+        UserJobInfo? userJobInfo = _entityFramework.UserJobInfo.Where(u => u.UserId == userId).FirstOrDefault();
+        if (userJobInfo != null)
+        {
+            return userJobInfo;
+        }
+
+        throw new Exception("Failed to Get User Job Info");
     }
 
     //If we would like to use Boolean and didn't have the save changes.
