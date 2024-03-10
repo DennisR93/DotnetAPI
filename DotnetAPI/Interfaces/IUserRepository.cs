22cda6b9702ee5b562ef5134aba0fa2682c376bd
dennisrabinovitch <rdennis0993@gmail.com>
2024-03-10 22:45:13 +0200

    adding method refs

--- DotnetAPI/Interfaces/IUserRepository.cs
+++ DotnetAPI/Interfaces/IUserRepository.cs
@@ -1,3 +1,5 @@
+using DotnetAPI.Models;
+
 namespace DotnetAPI.Interfaces;
 
 public interface IUserRepository
@@ -5,4 +7,10 @@ public interface IUserRepository
     public bool SaveChanges();
     public void AddEntity<T>(T addEntity);
     public void RemoveEntity<T>(T deleteEntity);
+
+    public IEnumerable<User> GetUsers();
+    public User GetSingleUser(int userId);
+
+    public UserSalary GetSingleUserSalary(int userId);
+    public UserJobInfo GetSingleUserJobInfo(int userId);
 }
