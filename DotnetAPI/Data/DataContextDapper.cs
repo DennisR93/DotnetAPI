c311647d6d96f9be213b47e75f3cd9433f1f14e2
dennisrabinovitch <rdennis0993@gmail.com>
2024-03-24 22:51:53 +0200

    adding two methods

--- DotnetAPI/Data/DataContextDapper.cs
+++ DotnetAPI/Data/DataContextDapper.cs
@@ -57,4 +57,16 @@ public class DataContextDapper
         
         return rowsAffected > 0;
     }
+    
+    public IEnumerable<T> LoadDataWithParameters<T>(string sql, DynamicParameters parameters)
+    {
+        IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+        return dbConnection.Query<T>(sql, parameters);
+    }
+    
+    public T LoadDataSingleWithParameters<T>(string sql, DynamicParameters parameters)
+    {
+        IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+        return dbConnection.QuerySingle<T>(sql, parameters);
+    }
 }
