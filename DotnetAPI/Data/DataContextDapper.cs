56c8ed3b9a7f7fd6d9d54ab588ca66f7f03df0cf
dennisrabinovitch <rdennis0993@gmail.com>
2024-03-26 00:42:01 +0200

    refactor

--- DotnetAPI/Data/DataContextDapper.cs
+++ DotnetAPI/Data/DataContextDapper.cs
@@ -37,25 +37,27 @@ public class DataContextDapper
         return dbConnection.Execute(sql);
     }
 
-    public bool ExecuteSqlWithParameters(string sql, List<SqlParameter> parameters)
+    public bool ExecuteSqlWithParameters(string sql, DynamicParameters parameters)
     {
+        IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+        return dbConnection.Execute(sql, parameters) > 0;
 
-        SqlCommand commandWithParams = new SqlCommand(sql);
-        foreach (SqlParameter parameter in parameters)
-        {
-            commandWithParams.Parameters.Add(parameter);
-        }
-        
-        SqlConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
-        dbConnection.Open();
-        
-        commandWithParams.Connection = dbConnection;
-
-        int rowsAffected = commandWithParams.ExecuteNonQuery();
-
-        dbConnection.Close();
-        
-        return rowsAffected > 0;
+        // SqlCommand commandWithParams = new SqlCommand(sql);
+        // foreach (SqlParameter parameter in parameters)
+        // {
+        //     commandWithParams.Parameters.Add(parameter);
+        // }
+        //
+        // SqlConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+        // dbConnection.Open();
+        //
+        // commandWithParams.Connection = dbConnection;
+        //
+        // int rowsAffected = commandWithParams.ExecuteNonQuery();
+        //
+        // dbConnection.Close();
+        //
+        // return rowsAffected > 0;
     }
     
     public IEnumerable<T> LoadDataWithParameters<T>(string sql, DynamicParameters parameters)
